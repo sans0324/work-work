b26c7e2a02c9995a8a4c55b7a23637bf17f83d8e
BGB <mw080522@gmail.com>
2023-12-07 21:43:14 +0900

    새로운 기능 제거

--- Project1/Project1/work.c
+++ Project1/Project1/work.c
@@ -1,41 +1,25 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
-#define MAX_LENGTH 100
+
 
 int main() {
-	char str[MAX_LENGTH];
+	char str[100];
 	char ch1, ch2;
 	int i;
-	printf("여러 글자를 입력 : ");
 
+	printf("Enter multiple characters :");
+	gets(str);
 
-	if (fgets(str, sizeof(str), stdin) == NULL) {
-		printf("Error reading input.\n");
-		return 1;
-	}
-
-	str[strcspn(str, "\n")] = '\0';
-
-	if (strlen(str) == MAX_LENGTH - 1 && str[MAX_LENGTH - 2] != '\n') {
-		printf("Input is too long.\n");
-		return 1;
-	}
-
-	printf("기존 문자와 새로운 문자를 입력 :");
-	if (scanf(" %c %c", &ch1, &ch2) != 2) {
-		printf("Error reading characters.\n");
-		return 1;
-	}
+	printf("Enter existing and new characters : ");
+	scanf("%c %c", &ch1, &ch2);
 
 	for (i = 0; i < strlen(str); i++) {
 		if (str[i] == ch1) {
 			str[i] = ch2;
 		}
-
 	}
-	printf("변환된 결과 ==> %s\n",str);
+	printf("a changed result ==> %s\n", str);
 
 	return 0;
-	
 }
